e59ff41a87a69ac0d8466da9443825869382f2e5
Krishnendu N G <ngkrishnendu1996@gmail.com>
2024-11-11 16:17:17 +0530

    second question

--- q2.py
+++ q2.py
@@ -1,2 +1,22 @@
-print("Hello world")
-print("Please")
+#1,Given a list of strings, group the strings that are anagrams of each other. Return the result as a list of lists, where each sublist contains words that are anagrams of each other.
+
+
+words = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+
+anagrams = {}
+
+for word in words:
+    
+    sorted_word = ''.join(sorted(word))
+    
+    
+    if sorted_word in anagrams:
+        anagrams[sorted_word].append(word)
+    else:
+        
+        anagrams[sorted_word] = [word]
+
+
+output = list(anagrams.values())
+print(output)
